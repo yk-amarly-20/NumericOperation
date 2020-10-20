b48396077ca68e1bb71bbe7da0938b0493a9c11b
yk-amarly-20 <y.k.83600260@icloud.com>
2020-10-21 02:10:22 +0900

    add: estimate pi

--- src/Arithmetic-geometric-mean/arithmetic_geometric_mean.py
+++ src/Arithmetic-geometric-mean/arithmetic_geometric_mean.py
@@ -12,16 +12,15 @@ class ArithmeticGeometricMean():
 
     """
 
-    def __init__(self, a_init, b_init, eps=0.001, verbose=False, iteration=10000):
+    def __init__(self, a_init, b_init, t_init, iteration=10):
 
-        self.eps = eps
-        self.verbose = verbose
         self.iteration = iteration
-
-        self.a = np.zeros(self.iteration + 2)
+        self.a = np.zeros(self.iteration + 1)
         self.a[0] = a_init
-        self.b = np.zeros(self.iteration + 2)
+        self.b = np.zeros(self.iteration + 1)
         self.b[0] = b_init
+        self.t = np.zeros(self.iteration)
+        self.t[0] = t_init
 
     def fit(self):
         """calculate mean
@@ -30,6 +29,11 @@ class ArithmeticGeometricMean():
         for i in range(self.iteration):
             self._calc_a(i)
             self._calc_b(i)
+
+        for j in range(self.iteration - 1):
+            self._calc_t(j)
+
+        return self.a, self.b
 
     def _calc_a(self, n):
         """calculate a
@@ -49,8 +53,35 @@ class ArithmeticGeometricMean():
 
         self.b[n + 1] = np.sqrt(self.a[n] * self.b[n])
 
+    def _calc_t(self, n):
+        """calculate t
+
+        Params:
+            n (int): the number of step
+        """
+
+        self.t[n + 1] = self.t[n] - self._calc_p(n) * (self.a[n] - self.a[n + 1]) ** 2
+
+    def _calc_p(self, n):
+        """calculate p
+
+        Params:
+            n (int): the number of step
+        """
+
+        return 2**n
+
     def show_result(self):
         """show result of calculation by graph
         """
 
-        plt.plot()
+        x = np.arange(self.iteration + 1)
+        plt.plot(x, self.a)
+        plt.plot(x, self.b)
+        plt.show()
+
+    def estimate_pi(self):
+        """estimate pi
+        """
+
+        return (self.a[-1] + self.b[-1]) ** 2 / (4 * self.t[-1])
